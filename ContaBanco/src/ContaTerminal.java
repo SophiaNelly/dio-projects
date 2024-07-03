8fbe0e1ef90682936f8fbb970b21f719e7550012
SophiaNelly <sophianellylb@gmail.com>
2024-07-03 00:18:22 -0300

    Banco

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,33 @@
+
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner scan = new Scanner(System.in);
+
+
+        System.out.println("Bem vindo(a) a nossa agencia. Por favor, digite os dados correspondentes: ");
+        
+
+        System.out.println("Digite o nome do cliente: ");
+        String nomeCliente = scan.nextLine();
+
+        System.out.println("Digite o número da Agência:");
+        String agencia = scan.nextLine();
+
+        System.out.println("Digite o numero da conta: ");
+        int numero = scan.nextInt();
+
+        System.out.println("Digite o valor do saldo: ");
+        double saldo = scan.nextDouble();
+
+        scan.close();
+
+        System.out.println("Olá, " + nomeCliente + ". Obrigada por criar uma conta em nosso banco. ");
+        System.out.println("Sua agência é: " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponivel para o saque.");
+
+
+       
     }
 }
